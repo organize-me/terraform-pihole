d369625de6b4f602f1bb3da6254b3a781f1b2db4
isaiah.v <isaiah.v@comcast.net>
2024-10-20 16:21:51 -0700

    backup script done

--- terraform/backup/pihole-backup.tf.sh
+++ terraform/backup/pihole-backup.tf.sh
@@ -9,14 +9,21 @@ TEMP_DIR_AWSCLI="/tmp/backup"
 
 # Clean up
 cleanup () {
+  echo "Cleanup Start"
+
   # Delete the temporary directory
   if [ -d "$TEMP_DIR_HOST" ]; then
     rm -rf "$TEMP_DIR_HOST"
   fi
+
+  echo "Cleanup Complete"
 }
 
 # Halt the script
 halt () {
+  # $1 - Exit code (optional)
+  # $2 - Message (optional)
+
   if [ -n "$1" ]; then
     EXIT_CODE=$1
   else
@@ -27,27 +34,35 @@ halt () {
 
   # Exit
   if [ $EXIT_CODE -ne 0 ]; then
-    echo "Backup Failed"
+    if [ -n "$2" ]; then
+      echo "Backup Failed: $2"
+    else
+      echo "Backup Failed"
+    fi
   fi
-  echo $EXIT_CODE
+  exit $EXIT_CODE
 }
 
 # Archive Pi-hole data
 archive_pi_hole_data () {
   # Archive Pi-hole data
+  echo "Archive Start"
 
-  mkdir -p "$TEMP_DIR_HOST" || halt 1
+  mkdir -p "$TEMP_DIR_HOST" || halt 1 "001 : Unable to create temporary directory"
 
   # Extract Pi-hole data
-  docker exec "${TF_PIHOLE_CONTAINER}" mkdir -p $TEMP_DIR_PIHOLE || halt 1
-  docker exec "${TF_PIHOLE_CONTAINER}" /usr/local/bin/pihole -a -t "$TEMP_DIR_PIHOLE/${TF_ARCHIVE_NAME}" || halt 1
-  docker cp "${TF_PIHOLE_CONTAINER}:$TEMP_DIR_PIHOLE/${TF_ARCHIVE_NAME}" "$TEMP_DIR_HOST/${TF_ARCHIVE_NAME}" || halt 1
-  docker exec "${TF_PIHOLE_CONTAINER}" rm -rf $TEMP_DIR_PIHOLE || halt 1
+  docker exec "${TF_PIHOLE_CONTAINER}" mkdir -p $TEMP_DIR_PIHOLE || halt 1 "002 : Unable to create temporary directory"
+  docker exec "${TF_PIHOLE_CONTAINER}" /usr/local/bin/pihole -a -t "$TEMP_DIR_PIHOLE/${TF_ARCHIVE_NAME}" || halt 1 "003 : Unable to create backup archive"
+  docker cp "${TF_PIHOLE_CONTAINER}:$TEMP_DIR_PIHOLE/${TF_ARCHIVE_NAME}" "$TEMP_DIR_HOST/${TF_ARCHIVE_NAME}" || halt 1 "004 : Unable to copy backup archive to host"
+  docker exec "${TF_PIHOLE_CONTAINER}" rm -rf $TEMP_DIR_PIHOLE || halt 1 "005 : Unable to remove temporary directory"
+
+  echo "Archive Complete"
 }
 
 # Upload to S3
 upload_archive_to_s3 () {
   # Upload to S3
+  echo "Upload Start"
 
   set -- "--rm"
   set -- "$@" "--name" "pihole_backup"
@@ -75,10 +90,9 @@ upload_archive_to_s3 () {
 
 
   CMD="s3 cp $TEMP_DIR_AWSCLI/${TF_ARCHIVE_NAME} s3://${TF_S3_BACKUP_BUCKET}/${TF_ARCHIVE_NAME}"
-  if ! docker run "$@" "${TF_AWS_CLI_IMAGE}" $CMD
-  then
-    halt 1
-  fi
+  docker run "$@" "${TF_AWS_CLI_IMAGE}" $CMD || halt 1 "006 : Unable to upload backup archive to S3"
+
+  echo "Upload Complete"
 }
 
 # Run the backup process
